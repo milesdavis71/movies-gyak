3a64b940436cc4e65d083747e2fb8a88aa52f26b
milesdavis71 <hevesiistvan71@hotmail.com>
2022-05-14 16:27:24 +0200

    Gyakorlás kész.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeaderComponent } from './shared/header/header.component'
 import { FooterComponent } from './shared/footer/footer.component'
 import { HomeComponent } from './pages/home/home.component'
 import { MoviesComponent } from './pages/movies/movies.component'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
